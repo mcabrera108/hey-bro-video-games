0ba393adb8a9161a866fa3835f6e6adaa78ecc58
Martin Cabrera <martin.cabrera103@gmail.com>
2024-03-25 07:52:21 -0500

    Added Responsiveness to Shop/About Page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -13,11 +13,9 @@ function About() {
             <span className={style.aboutBannerHeader}>About Us</span>
             <section className={style.aboutMainContainer}>
                 <div className={style.aboutDescriptionContainer}>
-                    <span>
-                        Hey Bro!! Video Games started with one man, Mousa.
-                        For as long as he could remember, video games were Mousa's passion which is why he began to sell video games from his garage at home.
-                        From humble beginnings, he expanded and grew the Hey Bro!! brand, relocating inside Traders Village back in 2017. Him and his team have been there ever since and hope to expand out in the near future!
-                    </span>
+                    Hey Bro!! Video Games started with one man, Mousa.
+                    For as long as he could remember, video games were Mousa's passion which is why he began to sell video games from his garage at home.
+                    From humble beginnings, he expanded and grew the Hey Bro!! brand, relocating inside Traders Village back in 2017. Him and his team have been there ever since and hope to expand out in the near future!
                 </div>
                 <div className={style.aboutBannerPhotoContainer}>
                     <img className={style.aboutImage} src="https://raw.githubusercontent.com/mcabrera108/shopping-cart-odin-project/main/src/assets/heybrovideogames_about.jpeg" alt="heybrovideogamesvintageimage" />
